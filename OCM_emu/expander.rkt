4966b003d7766961b6b29c181f4395a41d91d9fb
Nathan Fritzler <nfblaster@live.com>
2021-02-09 21:21:29 -0700

    Get an initial inspection to run without complaints.

--- OCM_emu/expander.rkt
+++ OCM_emu/expander.rkt
@@ -1,56 +1,50 @@
 #lang racket
-(require ;(for-syntax syntax/parse)
-         syntax/parse/define)
-(provide ;ocm-asm
-         ocm-asm-inst
+(require syntax/parse/define)
+(provide ocm-asm
          ocm-asm-dta
+         ocm-asm-inst
          ocm-asm-str
-         ;(rename-out [ocm-asm-mb #%module-begin])
-         )
-(define-simple-macro (ocm-asm-dta num:nat pound:boolean dta:nat nl:boolean)
+         (rename-out [ocm-asm-mb #%module-begin]))
+(define-simple-macro (ocm-asm-dta num:nat pound dta:nat nl)
                      (list (cons num dta)))
-(define-simple-macro (ocm-asm-inst num:nat colon:boolean inst:id nl:boolean)
-                     (list (cons num (inst->num inst))))
-#|(define-syntax-parser fn3
-    [(fn3 x:id rhs:expr)
-     #'(list )]
-    [(fn3 x:id y:id rhs:expr)
-     #'(lambda (x y) rhs)])|#
-#|(define-syntax-parser
+(define-simple-macro (ocm-asm-inst num:nat colon inst:id nl)
+                     ; inst is a reference to a number
+                     (list (cons num inst)))
+(define-simple-macro (ocm-asm-mb parse-tree:expr)
+                     (#%module-begin parse-tree))
+(define-syntax-parser
   ocm-asm-str
-  [(ocm-asm-str num:nat data:str nl:boolean)
-   #:fail-unless (= 1 (string-length (syntax-e data)))
-   #`(list (cons num #,(char->integer (string-ref (syntax-e #'data) 0))))]
-  [(ocm-asm-str start:nat sep:boolean end:nat data:str nl:boolean)
-   ;#:fail-unless (= ((syntax-e end) . - . (syntax-e start))
-   ;                 (string-length (syntax-e data)))
-   #`(list #,@(for/list ([num (in-range (syntax-e #'start) (syntax-e #'end))])
+  [(ocm-asm-str num:nat data:string nl)
+   #:fail-unless (= 1 (string-length (syntax-e #'data)))
+   "string length is not one in implicit char"
+   #`(list (cons num
+                 #,(char->integer
+                     (string-ref (syntax-e #'data) 0))))]
+  [(ocm-asm-str start:nat range end:exact-positive-integer data:string nl)
+   #:fail-unless (= ((syntax-e #'end) . - . (syntax-e #'start))
+                    (string-length (syntax-e #'data)))
+   "string ends are invalid"
+   #`(list #,@(for/list ([num (in-range (syntax-e #'start)
+                                        (syntax-e #'end))])
                         #`(cons #,num #,(char->integer
-                                          (string-ref (syntax-e #'data)
-                                                      num)))))])|#
-(define-syntax (ocm-asm-str syntax-object)
-  (syntax-case syntax-object ()
-               [(_ num data nl)
-                #`(list (cons num
-                              #,(char->integer
-                                  (string-ref (syntax-e #'data) 0))))]
-               [(_ num data nl)
-                #`(list #,@(for/list ([num (in-range (syntax-e #'start)
-                                                     (syntax-e #'end))])
-                                     #`(cons #,num #,(char->integer
-                                                       (string-ref
-                                                         (syntax-e #'data)
-                                                         num)))))]))
-
-#|(define-macro (jsonic-mb PARSE-TREE)
-  #'(#%module-begin
-     (define result-string PARSE-TREE)
-     (define validated-jsexpr (string->jsexpr result-string))
-     (display result-string)))
-(define-macro (jsonic-char CHAR-TOK-VALUE)
-  #'CHAR-TOK-VALUE)
-(define-macro (jsonic-program SEXP-OR-JSON-STR ...)
-  #'(string-trim (string-append SEXP-OR-JSON-STR ...)))
-(define-macro (jsonic-sexp SEXP-STR)
-  (with-pattern ([SEXP-DATUM (format-datum '~a #'SEXP-STR)])
-    #'(jsexpr->string SEXP-DATUM)))|#
+                                          (string-ref
+                                            (syntax-e #'data)
+                                            num)))))])
+#|(define-for-syntax (num->instruction num)
+                   (case num [else (lambda () #f)]))|#
+(define-simple-macro
+  (ocm-asm items:expr ...)
+  (let loop
+    ([numbers (map cdr (apply append (for/list ([item (list items ...)]
+                                                #:when (list? item))
+                                               item)))]
+     [pgm-counter 0]
+     [reg-A 0]
+     [reg-B 0]
+     [direction #t]
+     [overflow #f])
+    ((num->instruction (list-ref numbers pgm-counter))
+     ; read-only args
+     numbers pgm-counter reg-A reg-B direction overflow
+     ; Loopback arg
+     loop)))
