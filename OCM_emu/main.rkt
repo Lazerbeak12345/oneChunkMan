beb3b557c8b73e1fd194d719e7b2b3d872f11c62
Nathan Fritzler <nfblaster@live.com>
2021-02-16 22:14:57 -0700

    Remove some unnneded print statements

--- OCM_emu/main.rkt
+++ OCM_emu/main.rkt
@@ -1,11 +1,8 @@
 #!/usr/bin/env racket
 #lang racket/base
 (require "reader.rkt")
-(module+ reader
-         (println "reader module")
-         (provide read-syntax))
+(module+ reader (provide read-syntax))
 (module+ main
-         (println "main module")
          (require "private/fileInfo.rkt")
          (eval-syntax (read-syntax arg-path arg-port)))
 
