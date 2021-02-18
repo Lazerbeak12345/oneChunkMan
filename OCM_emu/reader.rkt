0e8dfeaee93a70cbeb85f85de7277fedd8a1d4e6
Nathan Fritzler <nfblaster@live.com>
2021-02-17 22:41:47 -0700

    Allow the reader to work in DrRacket

--- OCM_emu/reader.rkt
+++ OCM_emu/reader.rkt
@@ -1,7 +1,7 @@
 #lang racket/base
 (require racket/contract syntax/strip-context "tokenizer.rkt" "parser.rkt")
 (define/contract (read-syntax path port)
-                 (-> (or/c path-string? path?) input-port? syntax?)
+                 (-> any/c #|(or/c path-string? path?)|# input-port? syntax?)
                  (strip-context
                    (with-syntax ([PT (parse path (make-tokenizer port))])
                                 #'(module ocm-asm-module OCM_emu/expander
